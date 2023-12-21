2a2161bef50db64718cfcea077cabfd23c9061d3
Uygar Eren <komifb90@hotmail.com>
2023-12-21 14:45:04 +0300

    Login screen is ok

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function logCredentials() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+        
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    // Add your additional logic for handling the credentials here,
+    // such as sending them to a server or storing them locally.
+  }
